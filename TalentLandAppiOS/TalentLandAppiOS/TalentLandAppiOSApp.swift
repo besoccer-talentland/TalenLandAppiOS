e2fa1dca1ed98f0d94b98084c1e9a4f5d9949268
Jose Bechiarelli Mateo <josebechiarelli@besoccer.com>
2025-12-02 08:29:20 +0100

    Initial commit
    Initializes the Xcode project for the TalentLandAppiOS application.
    Adds basic project files, including assets, content view, and the app delegate.
    Configures build settings and project metadata.

---
+++ TalentLandAppiOS/TalentLandAppiOS/TalentLandAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalentLandAppiOSApp.swift
+//  TalentLandAppiOS
+//
+//  Created by BeSoccer on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TalentLandAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
